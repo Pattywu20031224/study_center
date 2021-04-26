c4da90ea9bcb970da3c03dae31d8c7ab93a0a67e
Pattywu20031224 <10835096@dcsh.tp.edu.tw>
2021-04-26 13:11:14 +0800

    redirectview

--- study_center/urls.py
+++ study_center/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import *
+from django.views.generic import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include("homepage.urls")),
+    path('', RedirectView.as_view(url="homepage/")),
     #path('student/', include("student.urls")),
     #path('teacher/', include("teacher.urls")),
     #path('log/', include("log.urls")),
